bb00c71df49d6d099463b6e242537c2f4a9ca63d
computer <computer@LAPTOP-H4H0L5SQ.mshome.net>
2022-11-02 13:37:59 +0000

    write tests covering all BoardState methods

--- src/test/java/com/github/ivanl2/chess/board/BoardStateTest.java
+++ src/test/java/com/github/ivanl2/chess/board/BoardStateTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 import com.github.ivanl2.chess.piece.Piece;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
 
 public class BoardStateTest {
 	
@@ -46,5 +50,46 @@ public class BoardStateTest {
 		assertThrows(OutOfBoardBoundsException.class, () -> board.addPiece(p2, 8, 8));
 		assertThrows(OutOfBoardBoundsException.class, () -> board.addPiece(p2, 0, -1));
 		assertThrows(OutOfBoardBoundsException.class, () -> board.addPiece(p2, -1, 5));
+	}
+	
+	@Test
+	public void testMovePiece() {
+		Piece p = mock(Piece.class);
+		board.addPiece(p, 0, 0);
+		assertTrue(board.movePiece(0, 0, 1, 1));
+		assertFalse(board.movePiece(0, 0, 1, 1));
+		assertEquals(board.getPiece(0, 0), null);
+		assertEquals(board.getPiece(1 ,1), p);
+		
+		Piece p2 = mock(Piece.class);
+		board.addPiece(p, 7, 7);
+		assertTrue(board.movePiece(7, 7, 1, 1));
+		assertEquals(board.getPiece(1, 1), p2);
+		assertNotEquals(board.getPiece(1, 1), p);
+		assertTrue(board.getAllPieces().length == 1); // piece taken
+	}
+	
+	@Test
+	public void testRemovePiece() {
+		Piece p = mock(Piece.class);
+		Piece p2 = mock(Piece.class);
+		board.addPiece(p, 0, 0);
+		board.addPiece(p2, 1, 5);
+		assertTrue(Arrays.asList(board.getAllPieces()).contains(p));
+		board.removePiece(0, 0);
+		assertEquals(board.getPiece(0, 0), null);
+		assertEquals(board.getAllPieces().length, 1);
+	}
+	
+	@Test
+	public void testGetAllPieces() {
+		for (int i=0;i<64;i++) {
+			board.addPiece(mock(Piece.class), i%8, i/8);
+		}
+		assertEquals(board.getAllPieces().length, 64);
+		for (int i=0;i<64;i++) {
+			board.removePiece(i%8, i/8);
+		}
+		assertEquals(board.getAllPieces().length, 0);
 	}
 }
